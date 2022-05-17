f748706b77270eda2694b6285be0b830ce0c14ea
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-05-17 14:54:06 -0700

    inital commit

---
+++ lab6.py
@@ -0,0 +1,13 @@
+
+
+"""Structured English
+To, start we will generate a random integer between one and twenty, and
+based on the result of the random number, we check to see if it falls under a certain range.
+If the number is greater than 15, then the result will be "Cherries"
+otherwise if the number is greater than 10 the result will be "Orange"
+otherwise if the number is greater than 5 the result will be "Melon
+otherwise if the number is greater than 1 the result will be "Bell"
+otherwise the result will be "Bar"
+We interate over useing a loop three times and print the results to the user. Ex "Cherries, Orange, Melon"
+
+"""
